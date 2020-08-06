c23bbf443e67b908ca303af3244e38b417b91727
Aadithya Vasudevan <68108339+Aadithya-19@users.noreply.github.com>
2020-08-06 14:38:29 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -1,34 +1,4 @@
-// class paper {
-//     constructor(x, y, width, height) {
-//       var options = {
-//         isStatic : false,
-//           'restitution':0.3,
-//           'friction':0.5,
-//           'density':1.2
-//       }
-//       this.body = Bodies.rectangle(x, y, width, height, options);
-//       this.width = width;
-//       this.height = height;
-//       this.Img = loadImage("Images/paper.png");
 
-
-//       World.add(world, this.body);
-//     }
-//     display(){
-//       var pos =this.body.position;
-//       var angle = this.body.angle;
-//       push();
-//       translate(pos.x, pos.y);
-//       rotate(angle);
-//       ellipseMode(RADIUS);
-//       strokeWeight(4);
-//       stroke("green");
-//       image(this.Img, 0, 0, this.width, this.height);
-//       fill(255);
-//       ellipse(0, 0, 20, 20);
-//       pop();
-//     }
-//   };
   
 
 class paper {
